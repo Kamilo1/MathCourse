c4d7179bf6773aeeb8bd5f23af444625d2be84ee
bnalepa <bnalepa@o2.pl>
2024-06-01 12:14:12 +0200

    Poprawa tłumaczenia

--- app/src/main/java/pl/wsei/pam/MainViewModel.kt
+++ app/src/main/java/pl/wsei/pam/MainViewModel.kt
@@ -4,15 +4,15 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
-    val departments = MutableLiveData(listOf("Arithmetic", "Algebra", "Geometry", "Calculus"))
+    val departments = MutableLiveData(listOf("Najpopularniejsze wzory", "Funkcje", "Ciągi", "Geometria"))
     val topics = MutableLiveData<List<String>>()
 
     fun updateTopics(department: String) {
         topics.value = when (department) {
-            "Algebra" -> listOf("Wzory skróconego mnożenia", "Quadratic Equations", "Polynomials")
-            "Arithmetic" -> listOf("Addition", "Subtraction", "Multiplication", "Division")
-            "Geometry" -> listOf("Triangles", "Circles", "Angles")
-            "Calculus" -> listOf("Limits", "Derivatives", "Integrals")
+            "Najpopularniejsze wzory" -> listOf("Wzory skróconego mnożenia", "Twierdzenie Pitagorasa", "Delta")
+            "Funkcje" -> listOf("Funkcja liniowa", "Funkcja kwadratowa", "Funkcja wykładnicza", "Funkcja logarytmiczna")
+            "Ciągi" -> listOf("Ciągi arytymetyczne", "Ciągi geometryczne")
+            "Geometria" -> listOf("Odcinek", "Okrąg", "Symetria")
             else -> emptyList()
         }
     }
